64d729f9087765da50626efd3ddadc9e03d09ec8
George Sandoval <george.sandoval.estela@gmail.com>
2023-12-31 01:32:40 -0500

    change java version to 19 and flyway configuration

--- src/main/java/com/nativa/reservation/ReservationApplication.java
+++ src/main/java/com/nativa/reservation/ReservationApplication.java
@@ -2,6 +2,7 @@ package com.nativa.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class ReservationApplication {

35104164b4a4e0f96c3b00451a6a11b3c82140ce
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-05 01:53:05 -0500

    show uuid in find All

--- src/main/java/com/nativa/reservation/domain/dto/response/CustomerResponseDTO.java
+++ src/main/java/com/nativa/reservation/domain/dto/response/CustomerResponseDTO.java
@@ -2,11 +2,13 @@ package com.nativa.reservation.domain.dto.response;
 
 import com.nativa.reservation.domain.dto.request.CustomerRequestDTO;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
+@Data
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor

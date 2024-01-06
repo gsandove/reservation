95a40b2df65d39fcbc74b9ac12390b6c752ef873
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-05 23:48:43 -0500

    feature methods find in repository

--- src/main/java/com/nativa/reservation/repository/StadiumRepository.java
+++ src/main/java/com/nativa/reservation/repository/StadiumRepository.java
@@ -3,7 +3,12 @@ package com.nativa.reservation.repository;
 import com.nativa.reservation.domain.Stadium;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StadiumRepository extends JpaRepository<Stadium, UUID> {
+
+    List<Stadium> findAllByDeletedAtIsNull();
+    Optional<Stadium> findByUuidAndDeletedAtIsNull(UUID uuid);
 }

386f0d9e37e0df0a6bc4e225ba6a00280d0e304c
Winston Barrera <wijubalo@outlook.com>
2024-01-22 20:07:20 -0500

    add snsClient

--- src/main/java/com/nativa/reservation/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/nativa/reservation/service/impl/NotificationServiceImpl.java
@@ -5,23 +5,32 @@ import io.awspring.cloud.sns.core.SnsTemplate;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
 
 @Slf4j
 @Component
 public class NotificationServiceImpl implements NotificationService {
     private final SqsTemplate sqsTemplate;
     private final SnsTemplate snsTemplate;
+    private final SnsClient snsClient;
 
-    public NotificationServiceImpl(SqsTemplate sqsTemplate, SnsTemplate snsTemplate) {
+    public NotificationServiceImpl(SqsTemplate sqsTemplate, SnsTemplate snsTemplate, SnsClient snsClient) {
         this.sqsTemplate = sqsTemplate;
         this.snsTemplate = snsTemplate;
+        this.snsClient = snsClient;
     }
 
     @Override
     public void send(String subject, String message) {
-        this.snsTemplate.sendNotification(message, subject);
-        this.sqsTemplate.send(message);
+        PublishRequest request = PublishRequest.builder()
+                .targetArn("arn:aws:sqs:us-west-2:674342301842:queue-test")
+                .message(message)
+                .subject(subject)
+                .build();
+        this.snsClient.publish(request);
     }
 
     @SqsListener("${queue.aws.reservation}")
